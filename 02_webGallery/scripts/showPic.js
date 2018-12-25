3f6f76c45d93e7c2520b6acfb6831e7087a45134
London <767439682@qq.com>
2018-12-25 14:44:02 +0800

    add the layout file and add the show title function

--- 02_webGallery/scripts/showPic.js
+++ 02_webGallery/scripts/showPic.js
@@ -1,6 +1,22 @@
 
 function showPic(whichPic) {
 	var source = whichPic.getAttribute("href");
+	
+	// show the picture
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
+	
+	// show the picture title
+	var text = whichPic.getAttribute("title");
+	var description = document.getElementById("description");
+	//alert(description.firstChild.nodeValue);
+	description.firstChild.nodeValue = text;
 }
+
+function countBodyChildren() {
+	var body_element = document.getElementsByTagName("body")[0];
+	//alert(body_element.childNodes.length);
+	//alert(body_element.nodeType);
+}
+
+window.onload = countBodyChildren;
