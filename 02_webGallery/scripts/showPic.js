306eca8d527836b5c04e1fd630cdc1751d2d8366
London <767439682@qq.com>
2018-12-25 16:22:19 +0800

    completely split html and js code apart and make it safer

--- 02_webGallery/scripts/showPic.js
+++ 02_webGallery/scripts/showPic.js
@@ -1,16 +1,23 @@
 
 function showPic(whichPic) {
+	
+	if (!document.getElementById("placeholder")) return false; // can't find 'placeholder' here
 	var source = whichPic.getAttribute("href");
 	
 	// show the picture
 	var placeholder = document.getElementById("placeholder");
 	placeholder.setAttribute("src", source);
 	
-	// show the picture title
-	var text = whichPic.getAttribute("title");
-	var description = document.getElementById("description");
-	//alert(description.firstChild.nodeValue);
-	description.firstChild.nodeValue = text;
+	if (document.getElementById("description")) {
+		// show the picture title
+		var text = whichPic.getAttribute("title");
+		var description = document.getElementById("description");
+		//alert(description.firstChild.nodeValue);
+		if (description.firstChild.nodeType == 3) {
+			description.firstChild.nodeValue = text;
+		}
+	}
+	return true;
 }
 
 function countBodyChildren() {
@@ -19,4 +26,23 @@ function countBodyChildren() {
 	//alert(body_element.nodeType);
 }
 
-window.onload = countBodyChildren;
+window.onload = function() {
+	countBodyChildren();
+	prepareGallery();
+}
+
+function prepareGallery() {
+	if (!document.getElementById) return false;
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	
+	for ( var i = 0; i < links.length; i ++) {
+		links[i].onclick = function() {
+			return !showPic(this);
+			// if showPic returns true, it means normally processed, no further operation needed. 
+		}
+	}
+}
